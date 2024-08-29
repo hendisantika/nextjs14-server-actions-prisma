d79ed764c2f63ee09a6cc3b1234589757e7ce374
hendisantika <hendisantika@yahoo.co.id>
2024-08-29 07:45:56 +0700

    Add updateTask in tasks-actions.tsx🫸🌀✏️📗🐧🐳⬆

--- src/actions/tasks-actions.tsx
+++ src/actions/tasks-actions.tsx
@@ -40,3 +40,27 @@ export async function removeTask(formData: FormData) {
 
     revalidatePath("/");
 }
+
+export async function updateTask(formData: FormData) {
+    const id = formData.get("id")?.toString();
+    const name = formData.get("name")?.toString();
+    const description = formData.get("description")?.toString();
+    const priority = formData.get("priority")?.toString();
+
+    if (!id || !name || !description || !priority) {
+        return;
+    }
+
+    await prisma.task.update({
+        where: {
+            id: parseInt(id),
+        },
+        data: {
+            name: name,
+            description: description,
+            priority: priority,
+        }
+    });
+
+    redirect("/");
+}
