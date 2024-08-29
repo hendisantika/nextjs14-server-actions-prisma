0208743bb50d5d19f5d783520027906a3bf886c5
hendisantika <hendisantika@yahoo.co.id>
2024-08-29 07:45:42 +0700

    Add removeTask in tasks-actions.tsx🫸🌀✏️📗🐧🐳⬆

--- src/actions/tasks-actions.tsx
+++ src/actions/tasks-actions.tsx
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import {redirect} from "next/navigation";
+import {revalidatePath} from "next/cache";
 
 export async function createTask(formData: FormData) {
     const name = formData.get("name")?.toString();
@@ -23,3 +24,19 @@ export async function createTask(formData: FormData) {
     redirect("/");
 }
 
+export async function removeTask(formData: FormData) {
+    "use server";
+    const taskId = formData.get("taskId")?.toString();
+
+    if (!taskId) {
+        return;
+    }
+
+    await prisma.task.delete({
+        where: {
+            id: parseInt(taskId),
+        },
+    });
+
+    revalidatePath("/");
+}
